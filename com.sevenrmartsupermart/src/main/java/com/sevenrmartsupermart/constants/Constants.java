cb6f82753625ec6672f3f284d71a64d2eb8f5037
Nafrin1 <nafrinb890@gmail.com>
2024-12-03 08:41:40 +0530

    comit last

--- com.sevenrmartsupermart/src/main/java/com/sevenrmartsupermart/constants/Constants.java
+++ com.sevenrmartsupermart/src/main/java/com/sevenrmartsupermart/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String CONFIG_FILE_PATH = System.getProperty("user.dir")
 			+ "//src//main//resources//config.properties";
 	public static final String EXCEL_FILE_PATH = System.getProperty("user.dir")
-			+ "//src//main//resources//ExcelFiles//";
+			+ "//src//main//resources//InputData//";
 	public static final String SCREENSHOT_FILE_PATH = System.getProperty("user.dir") + "//screenshots//";
 	public static final String EXTENT_REPORT_PATH = System.getProperty("user.dir") + "//ExtentReports";
 	public static final String SAMPLE_IMAGE_PATH=System.getProperty("user.dir") +"/src/main/resources/images/spongebob.jpg";
